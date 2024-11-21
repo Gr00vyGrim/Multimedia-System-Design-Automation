5f2cf60fa0f2d469162f8e150aa4d97758b5b53e
Gr00vyGrim <116598731+Gr00vyGrim@users.noreply.github.com>
2024-11-20 18:42:56 -0800

    adding command ling args example

---
+++ main.py
@@ -0,0 +1,24 @@
+'''
+Day 1:
+python3 COMP467_proj2.py --files import_files/Baselight_JJacobs_20230323.txt import_files/Flame_MFelix_20230323.txt import_files/Flame_DFlowers_20230323.txt --xytech import_files/Xytech_20230323.txt --verbose
+python3 COMP467_proj2.py --files Baselight_JJacobs_20230323.txt Flame_MFelix_20230323.txt Flame_DFlowers_20230323.txt --xytech Xytech_20230323.txt --verbose
+
+Day 2:
+python3 COMP467_proj2.py --files import_files/Baselight_TDanza_20230324.txt --xytech import_files/Xytech_20230324.txt  --verbose
+python3 COMP467_proj2.py --files Baselight_TDanza_20230324.txt --xytech Xytech_20230324.txt  --verbose
+
+Day 3:
+python3 COMP467_proj2.py --files import_files/Baselight_GLopez_20230325.txt --xytech import_files/Xytech_20230325.txt  --verbose
+python3 COMP467_proj2.py --files Baselight_GLopez_20230325.txt --xytech Xytech_20230325.txt  --verbose
+
+Day 4:
+python3 COMP467_proj2.py --files import_files/Baselight_BBonds_20230326.txt import_files/Flame_DFlowers_20230326.txt --xytech import_files/Xytech_20230326.txt  --verbose
+python3 COMP467_proj2.py --files Baselight_BBonds_20230326.txt Flame_DFlowers_20230326.txt --xytech Xytech_20230326.txt  --verbose
+
+
+Day 5:
+python3 COMP467_proj2.py --files import_files/Baselight_THolland_20230327.txt import_files/Flame_BBonds_20230327.txt --xytech import_files/Xytech_20230327.txt  --verbose
+python3 COMP467_proj2.py --files Baselight_THolland_20230327.txt Flame_BBonds_20230327.txt --xytech Xytech_20230327.txt  --verbose
+
+'''
+
